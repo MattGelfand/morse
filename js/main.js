c0b7de073545b0508d68281135be9e85f9d7ea21
MattGelfand <matthew.gelfand@gmail.com>
2014-11-13 11:23:06 -0500

    add chitown img

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-$("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });
+/*$("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });*/
+
+
 
 $('a').smoothScroll();
 
+
+
+
+
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
