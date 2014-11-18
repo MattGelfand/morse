26a0704fa7cea721f869e1c16e675a9b584021d8
MattGelfand <matthew.gelfand@gmail.com>
2014-11-18 17:37:05 -0500

    add so many images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 /*$("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });*/
 
 
+ 
+// Picture element HTML5 shiv
+document.createElement( "picture" );
 
 
 /* Smooth Scrollage */
